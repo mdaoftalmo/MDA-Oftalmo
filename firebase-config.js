48503e12cbc597e6aff6a41dcf6747d14350e6e3
mdaoftalmo <leo.sht@gmail.com>
2025-07-02 11:59:34 -0300

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
 // firebase-config.js
 const firebaseConfig = {
-  apiKey: "AIzaSyAFYfZTSX3oCM-KMpHTOCJjxyXx6AzWtdw",
-  authDomain: "clinica-e2cee.firebaseapp.com",
-  projectId: "clinica-e2cee",
-  storageBucket: "clinica-e2cee.firebasestorage.app",
-  messagingSenderId: "522736936878",
-  appId: "1:522736936878:web:8137bbcb790d8cd8e38a3f"
-};
+  apiKey: AIzaSyAFYfZTSX3oCM-KMpHTOCJjxyXx6AzWtdw,
+  authDomain: clinica-e2cee.firebaseapp.com,
+  projectId: clinica-e2cee,
+  storageBucket: clinica-e2cee.firebasestorage.app,
+  messagingSenderId: 522736936878,
+  appId: 1:522736936878:web:8137bbcb790d8cd8e38a3f
+}
 
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
